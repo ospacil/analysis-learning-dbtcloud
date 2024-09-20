0d83a0ce2879c61094513c876690de76b11b75ea
Oldřich Spáčil <xspacil1@gmail.com>
2024-09-20 18:09:28 +0000

    A more complex query with references. Delete default examples.

--- models/stripe_payments_by_coupon.sql
+++ models/stripe_payments_by_coupon.sql
@@ -1,5 +1,3 @@
-{{ config(materialized='table') }}
-
 SELECT *
   FROM hive_metastore.default.stripe_payments
  WHERE PAYMENTMETHOD = 'coupon'
---
+++ models/users_who_payed_by_coupon.sql
@@ -0,0 +1,16 @@
+{{ config(materialized='table') }}
+
+WITH coupon_user_ids
+  AS (
+
+SELECT *
+  FROM {{ ref('user_ids_with_payment_by_coupon')}}
+)
+
+SELECT jaffle_shop_customers.*,
+       coupon_user_ids.num_of_success_orders,
+       coupon_user_ids.num_of_total_orders,
+       coupon_user_ids.total_success_amount
+  FROM hive_metastore.default.jaffle_shop_customers
+  JOIN coupon_user_ids ON coupon_user_ids.USER_ID = jaffle_shop_customers.ID
+  ORDER BY total_success_amount DESC
